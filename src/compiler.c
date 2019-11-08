c8a1380d86991b0b65b8fa2868159e49fe3abe76
Sergey Sushilin <sushilinsergey@gmail.com>
2019-11-09 01:00:27 +0300

    optimizated write_file()

--- src/compiler.c
+++ src/compiler.c
@@ -21,8 +21,11 @@
 
 #include <assert.h>
 #include <error.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "system.h"
 
@@ -44,7 +47,7 @@ str_append (char **str, size_t *length, const char *format, ...)
   va_end (argp);
 
   if (err < 0)
-    die (EXIT_FAILURE, errno, "vsprintf");
+    die (EXIT_FAILURE, errno, "vsprintf()");
 
   size_t formatted_str_len = strlen (formatted_str);
   *str = xrealloc (*str, *length + formatted_str_len + 1);
@@ -57,16 +60,40 @@ write_file (const char *filename,
             const char *source,
             const size_t source_length)
 {
-  FILE *fp;
-  if ((fp = fopen (filename, "w")) != NULL
-      && fwrite (source, sizeof (char), source_length, fp) == source_length
-      && fclose (fp) == 0 ? true : (fp = NULL, false))
-    return 0;
+  int fd;
+  if ((fd = open (filename, O_WRONLY)) >= 0)
+    {
+      char const *begin = source;
+      char const *const end = begin + source_length;
+
+      while (begin < end)
+        {
+          size_t remaining = end - begin;
+          ssize_t res = write (fd, begin, remaining);
+
+          if (res >= 0)
+            {
+              begin += res;
+              continue; /* Let's send the remaining part of this message.  */
+            }
+          if (errno == EINTR)
+            {
+              /* It is just a signal, try again.  */
+              continue;
+            }
+          else
+            {
+              /* It is a real error.  */
+              error (0, errno, "%s", quotef (filename));
+              return -1;
+            }
+        }
+
+      if (close (fd) == 0)
+        return 0;
+    }
 
   error (0, errno, "%s", quotef (filename));
-  if (fp != NULL && fclose (fp) != 0)
-    error (0, errno, "%s", quotef (filename));
-
   return -1;
 }
 
